ae539ba771ea7ff50f11f3d1a3e79c7633b758bf
alexbonavila <alexbonavila44@gmail.com>
2023-11-04 12:07:28 +0100

    Improving harcoded crud

--- src/Databases/Pdo/index.php
+++ src/Databases/Pdo/index.php
@@ -7,72 +7,120 @@ require_once 'Actions/Update.php';
 require_once 'Actions/Setup.php';
 
 
+function displayEntity($entity, $type)
+{
+    if ($entity) {
+        echo "$type ID: {$entity['id']}, ";
+
+        switch ($type) {
+            case 'User':
+                echo "Name: {$entity['name']}, Email: {$entity['email']}" . PHP_EOL;
+                break;
+            case 'Document':
+                echo "DNI: {$entity['dni']}, User ID: {$entity['user_id']}" . PHP_EOL;
+                break;
+            case 'Post':
+                echo "Name: {$entity['name']}, Text: {$entity['text']}, User ID: {$entity['user_id']}" . PHP_EOL;
+                break;
+        }
+    } else {
+        echo "$type not found." . PHP_EOL;
+    }
+}
+
+
 function create(Create $creator): void
 {
-    $userData = [
-        'name' => 'John Doe',
-        'email' => 'john.doe@example.com'
+    $users = [
+        [
+            "name" => "John Doe",
+            "email" => "john.doe@example.com",
+            "document" => "45789123A",
+            "posts" => [
+                ["name" => "Transform", "text" => "Creative Spaces"],
+                ["name" => "Growth", "text" => "Healthy Habits"],
+                ["name" => "Innovate", "text" => "Mindful Living"],
+            ],
+        ],
+        [
+            "name" => "Mike Doe",
+            "email" => "mike.doe@example.com",
+            "document" => "12456789S",
+            "posts" => [
+                ["name" => "Nourish", "text" => "Future Trends"],
+                ["name" => "Simplify", "text" => "Quiet Moments"],
+            ],
+        ],
+        [
+            "name" => "Jane Doe",
+            "email" => "jane.doe@example.com",
+            "document" => "78456123D",
+            "posts" => [
+                ["name" => "Balance", "text" => "Bold Choices"],
+            ],
+        ],
     ];
 
-    $userId = $creator->insertData('user', $userData);
 
-    if ($userId) {
-        echo "Inserted user with ID: $userId" . PHP_EOL;
-    } else {
-        echo "Failed to insert user" . PHP_EOL;
+    foreach ($users as $user) {
+        $user_id = $creator->insertData("user", ["name" => $user["name"], "email" => $user["email"]]);
+        $creator->insertData("document", ["dni" => $user["document"], "user_id" => $user_id]);
+
+        foreach ($user["posts"] as $post) {
+            $creator->insertData("post", ["name" => $post["name"], "text" => $post["text"], "user_id" => $user_id]);
+        }
     }
 }
 
 function read(Read $reader): void
 {
-    // Get all users
-    $users = $reader->getAll('user');
-    foreach ($users as $user) {
-        echo 'User ID: ' . $user['id'] . ', Name: ' . $user['name'] . ', Email: ' . $user['email'] . PHP_EOL;
+    $entities = [
+        'user' => 'User',
+        'document' => 'Document',
+        'post' => 'Post',
+    ];
+
+    foreach ($entities as $tableName => $entityType) {
+        $entities = $reader->getAll($tableName);
+        foreach ($entities as $entity) {
+            displayEntity($entity, $entityType);
+        }
+
+        // Get a single entity by ID
+        $entity = $reader->getById($tableName, 1);
+        displayEntity($entity, $entityType);
     }
 
-    // Get a single user by ID
-    $user = $reader->getById('user', 1);
-    if ($user) {
-        echo 'User ID: ' . $user['id'] . ', Name: ' . $user['name'] . ', Email: ' . $user['email'] . PHP_EOL;
-    } else {
-        echo "User not found.". PHP_EOL;
-    }
-
-    // Get a single user by email
-    $user = $reader->getByField('user', 'email', "john.doe@example.com");
-    if ($user) {
-        echo 'User ID: ' . $user['id'] . ', Name: ' . $user['name'] . ', Email: ' . $user['email'] . PHP_EOL;
-    } else {
-        echo "User not found.". PHP_EOL;
-    }
+    echo PHP_EOL . "==========================" . PHP_EOL . PHP_EOL;
 }
 
-function update(Update $updater): void
+function update(Update $updater, Read $reader): void
 {
-    $userId = 1;
     $userUpdateData = [
-        'name' => 'Jane Doe'
+        'email' => 'jane.doeeeeeeeeeeee83@example.com'
     ];
+
+    $user = $reader->getByField('user', 'email', "jane.doe@example.com");
+    $userId = $user['id'];
 
     $rowsAffected = $updater->updateById('User', $userId, $userUpdateData);
 
     if ($rowsAffected) {
-        echo "Updated {$rowsAffected} row(s).". PHP_EOL;
+        echo "Updated {$rowsAffected} row(s)." . PHP_EOL;
     } else {
-        echo "No rows updated or error occurred.". PHP_EOL;
+        echo "No rows updated or error occurred." . PHP_EOL;
     }
 }
 
-function delete(Delete $deleter):void
+function delete(Delete $deleter): void
 {
     $userId = 1;
     $rowsAffected = $deleter->deleteById('User', $userId);
 
     if ($rowsAffected) {
-        echo "Deleted {$rowsAffected} row(s).". PHP_EOL;
+        echo "Deleted {$rowsAffected} row(s)." . PHP_EOL;
     } else {
-        echo "No rows deleted or error occurred.". PHP_EOL;
+        echo "No rows deleted or error occurred." . PHP_EOL;
     }
 }
 
@@ -88,18 +136,18 @@ function main(): void
         $reader = new Read($pdo);
         $updater = new Update($pdo);
 
-        create($creator);
+        create($creator, $reader);
 
         read($reader);
 
-        update($updater);
+        update($updater, $reader);
 
         read($reader);
 
         delete($deleter);
 
         read($reader);
-        
+
     } else {
         echo "Execution finished with ERROR" . PHP_EOL;
     }
