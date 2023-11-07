51a05b3d971121d16db9bb49cf4f5e13638e8641
alexbonavila <alexbonavila44@gmail.com>
2023-11-07 20:38:58 +0100

    First connection and bugfix in composer

--- src/Databases/Mongo/bootstrap.php
+++ src/Databases/Mongo/bootstrap.php
@@ -1,3 +1,13 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use App\Config\Database;
+
+
+// Load environment variables
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+Database::initialize();
